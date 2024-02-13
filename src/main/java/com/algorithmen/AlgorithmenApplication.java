e4ae578e16cf3139f301391c3b9fe03db788aaa0
OlgaMac <oljamackeewa@gmail.com>
2024-02-13 21:53:02 +0300

    init

---
+++ src/main/java/com/algorithmen/AlgorithmenApplication.java
@@ -0,0 +1,13 @@
+package com.algorithmen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmenApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlgorithmenApplication.class, args);
+    }
+
+}
